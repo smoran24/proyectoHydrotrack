57b21ff2b7930385874a6657e3f3a1f0371bfb1c
Sebastián Alejandro Morán <81321955+smoran24@users.noreply.github.com>
2023-11-05 20:01:34 -0300

    actualizacion 5/11/23

--- websocket.js
+++ websocket.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const WebSocket = require('websocket').server;
+const mysql = require('mysql'); // Importa el paquete MySQL
 
 const server = http.createServer((request, response) => {
   // No es necesario implementar nada para HTTP en este ejemplo
@@ -9,15 +10,40 @@ const wsServer = new WebSocket({
   httpServer: server,
 });
 
+// Configura la conexión a tu base de datos MySQL
+const dbConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '013042',
+  database: 'hydrotrack',
+});
+
 wsServer.on('request', (request) => {
   const connection = request.accept(null, request.origin);
   
-  // Manejar mensajes recibidos
+  // Manejar mensajes recibos
   connection.on('message', (message) => {
     if (message.type === 'utf8') {
       console.log('Mensaje recibido:', message.utf8Data);
-      // Aquí puedes enviar una respuesta al cliente si es necesario
-      // connection.sendUTF('Respuesta: ' + message.utf8Data);
+
+      // Descompone el mensaje en sus elementos Flow y Volumen
+      const data = message.utf8Data.split(', ');
+      console.log("data",data);
+      const flow = parseFloat(data[0].split(' ')[1]);
+      console.log("flow",flow);      
+      const volume = parseFloat(data[1].split(' ')[1]);
+      console.log("volume",volume);      
+
+      // Inserta los datos en la tabla "mediciones" de MySQL
+      const insertQuery = `INSERT INTO mediciones (flow, volumen) VALUES (${flow}, ${volume})`;
+
+      dbConnection.query(insertQuery, (error, results, fields) => {
+        if (error) {
+          console.error('Error al insertar datos en MySQL:', error);
+        } else {
+          console.log('Datos almacenados en MySQL con éxito.');
+        }
+      });
     }
   });
 
@@ -31,4 +57,4 @@ wsServer.on('request', (request) => {
 const port = 8080; // Puedes cambiar el puerto si es necesario
 server.listen(port, () => {
   console.log('Servidor de WebSockets escuchando en el puerto', port);
-})
+});
